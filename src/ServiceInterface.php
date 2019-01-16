dc3c44a6c7af87525bd09dafe221342614053204
南山吴彦祖 <anoxia@engineer.com>
2019-01-16 17:43:23 +0800

    初步完成依赖注入组件

---
+++ src/ServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+namespace Bee\Di;
+
+interface ServiceInterface
+{
+    public function getName() : string;
+
+    public function setShared(bool $shared);
+
+    public function isShare() : bool;
+
+    public function setDefinition($definition);
+
+    public function getDefinition();
+
+    public function resolve($parameters = null, ContainerInterface $dependencyInjector = null);
+
+    public function setParameter(int $position, array $parameter) : ServiceInterface;
+
+    public static function __set_state(array $attributes) : ServiceInterface;
+}
